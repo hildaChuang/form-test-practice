e2bfb9258f5b22c15fc56ade32d758e5ea0ce55b
hilda.chuang <hilda.chuang@cimforce.com>
2021-12-23 13:35:13 +0800

    docs(day-4): test notes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+/**
+ * 測試集合 (Test suite)；作用域 (Scoping)
+ * @param 1 test suite description
+ * @param 2 test case
+ */
 describe('AppComponent', () => {
+  /**
+   * beforeAll - 在執行 所有 測試案例之前，會先執行這裡面的程式碼。
+   * beforeEach - 在執行 每一個 測試案例之前，會先執行這裡面的程式碼。
+   * afterAll - 在執行 所有 測試案例之後，會先執行這裡面的程式碼。
+   * afterEach - 在執行 每一個 測試案例之後，會先執行這裡面的程式碼。
+   */
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -10,6 +21,11 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  /**
+   * it: 測試案例 (Test case)
+   * @param 1 test case description
+   * @param 2 execute test case code
+   */
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
